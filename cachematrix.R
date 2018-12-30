3f1d524d2c2e31442a90dea5eeb317794288512b
ItheriaSam <45892435+ItheriaSam@users.noreply.github.com>
2018-12-30 14:56:08 +0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+rm(list = ls()) ## clear global environment
+## function 'makeChacheMatrix' creates a special matrix
 
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function()x
+setinv<-function(i) m<<- solve(x)
+getinv<-function() m
+list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
-
+## cachesolve functions solve the inverse of the 'special matrix' created above.
 cacheSolve <- function(x, ...) {
+  m<-x$getinv()
+  if(!is.null(m)){
+    
+    return(m)
+  }
+  
+  m<-solve(x$get())
+  x$setinv(m)
+  m
+  
         ## Return a matrix that is the inverse of 'x'
 }
+
